5954562c3adec5b1e39cfb06004cc73055aa2a5b
unknown <maurx18@gmail.com>
2018-08-26 22:50:15 +0200

    rendu

---
+++ Sources/Projet de vacances/Assets/scripts/characters/main/movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField]
+    private int walkSpeed ;
+
+
+    void Start()
+    {
+        walkSpeed = 5;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
+        }
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+}
